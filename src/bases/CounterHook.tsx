2e979fa61f64d7997e6d24355a97f46d3873b8fe
manucho <manuel.blanco77@gmail.com>
2021-12-23 14:58:30 -0300

    useRef, etc

--- src/bases/CounterHook.tsx
+++ src/bases/CounterHook.tsx
@@ -2,12 +2,12 @@ import { useCounter } from '../hooks/useCounter';
 
 export const CounterHook = () => {
 
-    const {counter, counterElement, handleClick} = useCounter();
+    const {counter, elementToAnimate, handleClick} = useCounter({maxCount: 15});
 
     return (
         <>
             <h1>CounterHook:</h1>
-            <h1 ref={ counterElement } className="mi-objeto"> { counter }</h1>
+            <h1 ref={ elementToAnimate } className="mi-objeto"> { counter }</h1>
 
             <button onClick={ handleClick }>
                 +1
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,38 +1,39 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import gsap from 'gsap';
+import { time } from 'console';
 
-const MAXIMUM_COUNT = 10;
-
-export const useCounter = () => {
+export const useCounter = ({ maxCount = 1 }) => {
 
     const [counter, setCounter] = useState(5);
     
-    const counterElement = useRef<HTMLHeadingElement>(null);
+    const elementToAnimate = useRef<any>(null);
+
+    const timeline = useRef(gsap.timeline());
 
     const handleClick = () => {
-        setCounter( prev => Math.min( prev + 1, MAXIMUM_COUNT ));
+        setCounter( prev => Math.min( prev + 1, maxCount ));
     }
 
+    useLayoutEffect(() => {
+
+        if (!elementToAnimate.current) return;
+        timeline.current.to(elementToAnimate.current, { y: -700, duration: 0.2, ease: 'ease.out'})
+            .to(elementToAnimate.current, { y: 0, duration: 1, ease: 'bounce.out' })
+    
+        timeline.current.repeat(7)
+
+    }, [])
+
     useEffect(() => {
-
-        if(counter < 10) return;
-
-        console.log('%cSe llego al valor maximo','color: red, background-color: black')
-        
-        const timeline = gsap.timeline();
-
-        timeline.to(counterElement.current, { y: -700, duration: 0.2, ease: 'ease.out'})
-            .to(counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' })
-
-        timeline.repeat(7)
-
+        if (counter < maxCount) return;
+        timeline.current.play(0)
 
     }, [counter]);
 
     return {
         counter,
         handleClick,
-        counterElement
+        elementToAnimate
     };
 
 };
